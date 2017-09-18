4afc353126eb30ae2f27372181452fa32346744b
Michael Witkovsky <michael.witkovsky@gmail.com>
2017-09-18 17:20:18 -0400

    progress

---
+++ Assets/Scripts/Spawning/PipeSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour {
+
+    public GameObject pipes;
+    public float yRange;
+    public float spawnDelay;
+
+    private float spawnTimer;
+
+    void Start()
+    {
+        spawnTimer = spawnDelay;
+    }
+	
+	void Update () {
+        if (GameManager.IsGameActive())
+        {
+            spawnTimer -= Time.deltaTime;
+            if (spawnTimer <= 0.0f)
+            {
+                Instantiate(pipes,
+                    transform.position + Vector3.up * Random.Range(-yRange, yRange),
+                    Quaternion.identity);
+                spawnTimer = spawnDelay;
+            }
+        }   
+	}
+}
